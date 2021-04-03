16f2caf4d8e046f54450a44cf32ead6c302b2541
zaquiel.rodriguez <zaquiel.rodriguez@datiobbva.com>
2021-04-03 21:50:45 +0200

    mvp vocabulary project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Formik } from 'formik';
+
 import './App.css';
+import { useFirebaseApp, useFirestore, useFirestoreCollection } from 'reactfire';
+import 'firebase/firestore';
 
 function App() {
+
+  const firebaseApp = useFirebaseApp();
+  const fireStore = useFirestore();
+  const wordsRef = firebaseApp.firestore().collection('words');
+  const documents = useFirestoreCollection(wordsRef);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <div className="word flex">
+            <span className="title" style={{flex: 1}}>ENGLISH</span>
+            <span className="title" style={{flex: 1}}>SPANISH</span>
+          </div>
+          {
+            documents.status === "success" ?
+              documents.data.docs.map((document) => {
+                const word = document.data();
+                return(
+                  <div className="word flex">
+                    <span className="wordName" style={{flex: 1}}>{word.englishName}</span>
+                    <span className="wordName" style={{flex: 1}}>{word.spanishName}</span>
+                  </div>
+                )
+              })
+              : null
+          }
+            <Formik
+              initialValues={{ englishName: '', spanishName: '' }}
+              validate={async (values) => {
+                if (values.englishName && values.englishName.length > 0) {
+                  // translate
+                }
+                const errors = {};
+                if (!values.englishName || values.englishName.length === 0) {
+                  errors.englishName = 'Required';
+                } else if (!values.spanishName || values.spanishName.length === 0)  {
+                  errors.spanishName = 'Required';
+                }
+                return errors;
+              }}
+              onSubmit={(values, { resetForm }) => {
+                fireStore.collection("words").doc().set({
+                  englishName: values.englishName,
+                  spanishName: values.spanishName,
+                });
+                resetForm();
+              }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <form onSubmit={handleSubmit} className="flex">
+                <input
+                  type="text"
+                  name="englishName"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.englishName}
+                  placeholder="Write english word..."
+                  className="actionWord"
+                />
+                <input
+                  type="text"
+                  name="spanishName"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.spanishName}
+                  placeholder="Write spanish word..."
+                  className="actionWord"
+                />
+                <button type="submit" disabled={isSubmitting} className="actionWord">
+                  Add word
+                </button>
+              </form>
+            )}
+          </Formik>
+        </header>
+      </div>
   );
 }
 
---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyCKIDEMkrj_NssjGuRf3fQ4yNo_c1N6gtY",
+  authDomain: "vocabulary-aaa99.firebaseapp.com",
+  databaseURL: "https://vocabulary-aaa99-default-rtdb.firebaseio.com",
+  projectId: "vocabulary-aaa99",
+  storageBucket: "vocabulary-aaa99.appspot.com",
+  messagingSenderId: "593747721785",
+  appId: "1:593747721785:web:dd4db875f8e95c0c0217e8",
+  measurementId: "G-97Q5CWYZN6"
+};
